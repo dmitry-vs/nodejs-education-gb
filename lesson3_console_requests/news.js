65719bfde7f19b2aa54354c5b0383a8dd8d8a09e
dmitry-vs <dmitryselin@inbox.ru>
2018-11-09 12:49:52 +0300

    Lesson 3 - added news.js

---
+++ lesson3_console_requests/news.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('https://yandex.ru', (err, response, body) => {
+    if(!err && response.statusCode === 200) {
+        news = getNews(body);
+        console.log('Yandex news headlines:');
+        for(let i in news) {
+            console.log(`${+i + 1}. ${news[i]}`);
+        }
+    }
+    else {
+        console.log('Failed to load page content');
+    }
+});
+
+// function to parse html and get news headlines
+let getNews = htmlContent => {
+    const $ = cheerio.load(htmlContent);
+    const newsAnchors = $('div#tabnews_newsc li a');
+
+    result = [];
+    for(let i = 0; i < newsAnchors.length; i++) {
+        result.push($(newsAnchors[i]).text());
+    }
+    return result;
+}
