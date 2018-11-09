b4bbf494bae1e12edfd0a19cefb2da659fae8e05
dmitry-vs <dmitryselin@inbox.ru>
2018-11-09 21:07:35 +0300

    Lesson 6 - added authentication

---
+++ lesson6_authentication/README.md
@@ -0,0 +1,14 @@
+# Authentication
+
+This folder contains "To Do List" express web application that supports authentication implemented in three different ways:
+* sessions and cookies
+* passport with local strategy
+* passport with github strategy
+
+Users and password hashes are stored in Mongo database.
+
+`bcrypt` module is used to handle password validation.
+
+---
+
+To use Github authentication, valid ClientID and ClientSecret are necessary.
---
+++ lesson6_authentication/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 1,
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+});
+
+UserSchema.methods.checkPassword = async function(password) {
+  const correct = await bcrypt.compare(password, this.passwordHash);
+  return correct;
+}
+
+module.exports = mongoose.model('User', UserSchema);
